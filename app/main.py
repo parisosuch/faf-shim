e4cbbcd08e0984e05ac5f1129193254c7194ca28
Paris Osuch <paris.osuch@gmail.com>
2026-03-15 11:33:33 -0700

    fix: reformat using ruff

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 
 from app.routers import shims, webhooks
 
-app = FastAPI(title="faf-shim", description="API shim for configuring webhook integrations")
+app = FastAPI(
+    title="faf-shim", description="API shim for configuring webhook integrations"
+)
 
 app.include_router(shims.router)
 app.include_router(webhooks.router)
